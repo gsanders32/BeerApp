9f71ea780028be29cf00682827fa4442f6f51b07
gsanders32 <gray.sanders@gmail.com>
2020-06-23 18:50:18 -0500

    beer table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeerTableComponent } from './beer-table/beer-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerTableComponent
   ],
   imports: [
     BrowserModule,
