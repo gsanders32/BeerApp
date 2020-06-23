110ce64530eb8d2f51a66d58f98b40289ee08663
gsanders32 <gray.sanders@gmail.com>
2020-06-23 18:45:48 -0500

    code for services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { IBeer } from '../interfaces/ibeer';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  url = 'https://api.punkapi.com/v2/';
+
+  async get(path, numb?){
+    let count: number;
+    if (numb === undefined){
+      count = 40;
+    } else{
+      count = numb;
+    }
+    return await this.httpClient.get<IBeer[]>(this.url + path + '?per_page=' + count).toPromise();
+  }
 }
--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
 
-  constructor() { }
+  constructor(private httpService: ApiService) { }
+
+  async getAll(count: number){
+    const data = await this.httpService.get('beers', count);
+    return data;
+  }
 }
